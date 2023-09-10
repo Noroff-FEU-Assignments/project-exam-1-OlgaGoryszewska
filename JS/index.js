16a17513369873a8bea3c614b247842230fdf0be
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2023-09-10 12:49:05 +0200

    git push

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,30 @@
-const apiBase = "https://semester69.wordpress.com/";
-const postsBase = "/wp-json/wp/v2/posts";
-const fullPostsUrl =`${apiBase}${postsBase}`;
+const apiBase = "https://public-api.wordpress.com/wp/v2/sites/semester69.wordpress.com/posts";
+const carouselWrapper= document.querySelector(".wrapper-carousel");
+const previewButton= document.querySelector(".preview-button");
+const nextButton= document.querySelector(".next-button");
 
-function fetchLatestsPosts(){
-    fetch (fullPostsUrl)
-    .then((response) =>{ if (!response.ok){ throw new Error('not fetching!');
+let currentIndex = 0;
 
-return response.jason();
-})
-
+ async function fetchPosts(){
+    try {
+    const response = await fetch(apiBase);
+    if (!response.ok){
+        throw new Error ('HTTP error! Status: ${response.status');
+    }
+    const posts = await response.json();
+    return posts;
+    } catch(error) {
+    console.error("Error fetching latest posts:", error);
+    throw error;
+    }   
 }
+fetchPosts()
+    .then(posts => {
+      console.log("Latest Posts:", posts);
+    })
+    .catch(error => {
+    });
+
+
 
 

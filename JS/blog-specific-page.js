3b3590c6392c3095ccb3aebd487ed04b3e4f96d2
OlgaGoryszewska <112639261+OlgaGoryszewska@users.noreply.github.com>
2023-09-14 08:26:00 +0200

    git push

---
+++ JS/blog-specific-page.js
@@ -0,0 +1,56 @@
+const blogContainer = document.querySelector(".blog-specific-page-container");
+
+async function getBlogPost() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const postId = urlParams.get("id");
+    if (!postId) {
+    
+        console.error("Missing post ID in URL");
+        return;
+    }
+
+    const apiBase = `https://public-api.wordpress.com/wp/v2/sites/semester69.wordpress.com/posts/${postId}?_embed`;
+
+    try {
+        const response = await fetch(apiBase);
+        if (!response.ok) {
+
+            console.error("Error fetching blog post:", response.status);
+            return;
+        }
+
+        const post = await response.json();
+
+    
+        const title = document.createElement("h1");
+        title.innerText = post.title.rendered;
+        title.style.fontSize ="18px";
+        title.style.paddingLeft = "0rem";
+        blogContainer.appendChild(title);
+
+
+        
+
+        if (post._embedded && post._embedded["wp:featuredmedia"]) {
+            const featuredImage = post._embedded["wp:featuredmedia"][0];
+            const img = document.createElement("img");
+            img.src = featuredImage.source_url;
+            img.alt = featuredImage.alt_text;
+            img.style.minWidth =" 200px";
+            img.style.height = "248px";
+            img.style.objectFit ="cover";
+            img.style.display ="flex";
+            blogContainer.appendChild(img);
+        }
+
+        const content = document.createElement("div");
+        content.innerHTML = post.content.rendered;
+        blogContainer.appendChild(content);
+
+    } catch (error) {
+        console.error("Error fetching and rendering blog post:", error);
+
+    }
+}
+
+getBlogPost();
--- JS/index.js
+++ JS/index.js
@@ -1,28 +1,4 @@
 'use strict'
 
 
-const apiBase = "https://public-api.wordpress.com/wp/v2/sites/semester69.wordpress.com/posts";
-
-
- async function fetchPosts(){
-    try {
-    const response = await fetch(apiBase);
-    if (!response.ok){
-        throw new Error ('HTTP error! Status: ${response.status}');
-    }
-    const posts = await response.json();
-    return posts;
-    } catch(error) {
-    console.error("Error fetching latest posts:", error);
-    throw error;
-    }   
-}
-fetchPosts()
-.then((posts) => {
-    console.log("Latest posts:", posts);
-
-})
-.catch((error) => {
-    console.error("Error in fetchPosts:", error);
-});
 
